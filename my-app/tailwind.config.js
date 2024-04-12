19037c13e227408e5b7f5ee3d882ac537e96722e
Kevin Kruijthof <k.kruijthof@student.tudelft.nl>
2024-04-12 10:07:34 +0200

    Create next app

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,25 @@
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      'void': '#0E0B16',
+      'fuschia': '#A239CA',
+      'jewel': '#4717F6',
+      'stark': '#E7DFDD'
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;

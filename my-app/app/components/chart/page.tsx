f059de22548a31644755f674ccd13926431c8fe4
Kevin Kruijthof <k.kruijthof@student.tudelft.nl>
2024-06-17 20:47:13 +0200

    color randomizer

--- my-app/app/components/chart/page.tsx
+++ my-app/app/components/chart/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/app/lib/types";
 import "chart.js/auto";
 import dynamic from "next/dynamic";
+import _ from 'underscore';
 
 export default function Chart(props: {
   costGroups: [CostGroupPair] | [];
@@ -35,6 +36,24 @@ export default function Chart(props: {
       ssr: false,
     }
   );
+
+  function randomHexCodeConverter(code: string) {
+    let hexcode = code.replace("#", "");
+    let r = parseInt(hexcode.substring(0, 2), 16);
+    let g = parseInt(hexcode.substring(2, 4), 16);
+    let b = parseInt(hexcode.substring(4, 6), 16);
+    let randomR = Math.abs(_.random(0,1) * 40 + (r - 20));
+    let randomG = Math.abs(_.random(0,1)  * 40 + (g - 20));
+    let randomB = Math.abs(_.random(0,1)  * 40 + (b - 20));
+    return (
+      "#" + ((1 << 24) + (randomR << 16) + (randomG << 8) + randomB).toString(16).slice(1)
+    );
+  }
+
+  function componentToHex(c: number) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
 
   const COLORS = [
     "#4dc9f6",
@@ -129,8 +148,8 @@ export default function Chart(props: {
             budget: -1,
           },
         },
-        second: 0
-      }
+        second: 0,
+      };
 
       const temp: [CostItemPair] = [leftOver];
       props.costItems.every((c) => temp.push(c));
@@ -141,7 +160,7 @@ export default function Chart(props: {
           {
             label: "Amount",
             data: temp.map((c) => c.first.amount),
-            backgroundColor: COLORS,
+            backgroundColor: temp.map((c) => randomHexCodeConverter(c.first.costGroup.hexcode)),
             borderWidth: 2,
           },
         ],
@@ -153,7 +172,7 @@ export default function Chart(props: {
           {
             label: "Amount",
             data: props.costItems.map((c) => c.first.amount),
-            backgroundColor: COLORS,
+            backgroundColor: props.costItems.map((c) => randomHexCodeConverter(c.first.costGroup.hexcode)),
             borderWidth: 2,
           },
         ],
